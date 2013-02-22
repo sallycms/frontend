40494f01ecb66fb1bf7519b7e6bd27c6943230ca
Christoph <github@xrstf.de>
2013-02-22 02:46:01 +0100

    error handling is now app-specific

--- lib/sly/App/Frontend.php
+++ lib/sly/App/Frontend.php
@@ -18,8 +18,12 @@ class sly_App_Frontend extends sly_App_Base {
 
 	public function initialize() {
 		$container = $this->getContainer();
-		$request   = $container->getRequest();
-		$isSetup   = sly_Core::isSetup();
+
+		// init basic error handling
+		$container->getErrorHandler()->init();
+
+		$request = $container->getRequest();
+		$isSetup = sly_Core::isSetup();
 
 		// Setup?
 		if (!$request->get->has('sly_asset') && $isSetup) {
